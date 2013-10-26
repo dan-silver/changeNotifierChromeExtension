c7bb347a46273c6a9ae13be52c4b7bd6a259423a
Dan Silver <dannysilver3@gmail.com>
2013-10-26 14:55:16 -0500

    Launch script into page

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,3 +1,7 @@
-setInterval(function() {
-  console.log("it works")
-}, 1000)
+console.log("The background page has been launched")
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+  if (request.action == "new_section") {
+    console.log('new_section action received!')
+    chrome.tabs.executeScript(null, {file: "assets/js/content_script_get_section.js"}) //null defaults to currently selected tab
+  }
+})
--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
 function RefreshCtrl($scope) {
   $scope.sections = JSON.parse(localStorage.getItem('sections'))
+  $scope.new_section = function() {
+  	chrome.runtime.sendMessage({action: "new_section"})
+  	window.close()
+  }
 }
