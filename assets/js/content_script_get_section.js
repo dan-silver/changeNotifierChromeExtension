b8075523a11b3298f2c70c51f02a1711ccb50f2b
Dan Silver <dannysilver3@gmail.com>
2013-10-26 16:22:17 -0500

    Clean

--- assets/js/content_script_get_section.js
+++ assets/js/content_script_get_section.js
@@ -1,21 +1,27 @@
 console.log('INJECTED!')
+
+var currentElement
+
 $(function () {
   $('*').hover(
     function(e){
-    	$("*").removeClass('updateNotifier-selected')
-        $(this).addClass('updateNotifier-selected')
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
+    	currentElement = $(this)
+    	$("*").removeClass('updateNotifierSelected')
+        $(this).addClass('updateNotifierSelected')
+        e.preventDefault()
+        e.stopPropagation()
+        return false
     },function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
+        e.preventDefault()
+        e.stopPropagation()
+        return false
     }
   )
   $(document).keypress(function(e) {
+      console.log('A key has been pressed!')
     if(e.which == 13) {
-      alert('You pressed enter!')
+      console.log('You pressed enter!')
+      console.log(currentElement.getFullPath())
       chrome.runtime.sendMessage({action: "create_new_section", sectionData:{}})
     }
   })
