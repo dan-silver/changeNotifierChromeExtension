0603fde5a2728541c7121f0e785eadbd632b2667
Dan Silver <dannysilver3@gmail.com>
2013-10-26 15:21:21 -0500

    Hover styling

--- assets/js/background.js
+++ assets/js/background.js
@@ -2,6 +2,8 @@ console.log("The background page has been launched")
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
   if (request.action == "new_section") {
     console.log('new_section action received!')
-    chrome.tabs.executeScript(null, {file: "assets/js/content_script_get_section.js"}) //null defaults to currently selected tab
+    chrome.tabs.executeScript(null, {file: "libraries/jquery.js"}) //null defaults to currently selected tab
+    chrome.tabs.executeScript(null, {file: "assets/js/content_script_get_section.js"})
+    chrome.tabs.insertCSS(null, {file: "assets/css/content_script_styles.css"})
   }
 })
--- assets/js/content_script_get_section.js
+++ assets/js/content_script_get_section.js
@@ -1,1 +1,21 @@
 console.log('INJECTED!')
+$(function () {
+  $('*').hover(
+    function(e){
+    	$("*").removeClass('updateNotifier-selected')
+        $(this).addClass('updateNotifier-selected')
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    },function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    }
+  )
+  $(document).keypress(function(e) {
+    if(e.which == 13) {
+      alert('You pressed enter!')
+    }
+  })
+})
