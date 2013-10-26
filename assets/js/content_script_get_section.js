ebfb4bd25c2429299f7d7c621d3725ee23947a1d
Ryan Endacott <rzeg24@gmail.com>
2013-10-26 16:30:16 -0500

    working full path

--- assets/js/content_script_get_section.js
+++ assets/js/content_script_get_section.js
@@ -5,7 +5,7 @@ var currentElement
 $(function () {
   $('*').hover(
     function(e){
-    	currentElement = $(this)
+    	currentElement = this
     	$("*").removeClass('updateNotifierSelected')
         $(this).addClass('updateNotifierSelected')
         e.preventDefault()
@@ -21,8 +21,36 @@ $(function () {
       console.log('A key has been pressed!')
     if(e.which == 13) {
       console.log('You pressed enter!')
-      console.log(currentElement.getFullPath())
+      console.log(currentElement);
+      console.log(getFullPath(currentElement))
       chrome.runtime.sendMessage({action: "create_new_section", sectionData:{}})
     }
   })
 })
+
+
+
+function getFullPath(el) {
+  if (!(el instanceof Element))
+      return 'hello world';
+  var path = [];
+  while (el.nodeType === Node.ELEMENT_NODE) {
+      var selector = el.nodeName.toLowerCase();
+      if (el.id) {
+          selector += '#' + el.id;
+          path.unshift(selector);
+          break;
+      } else {
+          var sib = el, nth = 1;
+          while (sib = sib.previousElementSibling) {
+              if (sib.nodeName.toLowerCase() == selector)
+                 nth++;
+          }
+          if (nth != 1)
+              selector += ":nth-of-type("+nth+")";
+      }
+      path.unshift(selector);
+      el = el.parentNode;
+  }
+  return path.join(" > ");
+}
