1434a9c11a7f1758bdd6bf53d1e404392fc2df70
Dan Silver <dannysilver3@gmail.com>
2013-10-26 20:10:37 -0500

    Let's see if Ryan notices

--- assets/js/content_script_get_section.js
+++ assets/js/content_script_get_section.js
@@ -19,15 +19,15 @@ var currentElement, selecting = true
   $(document).keypress(function(e) {
     if(e.which == 13 && selecting) {
       removeAllHighlights()
-      var dom_path = getFullPath(currentElement);
-      var content = $(currentElement).html();
-      console.log(currentElement);
-      console.log(dom_path);
+      var dom_path = getFullPath(currentElement)
+      var content = $(currentElement).html()
+      console.log(currentElement)
+      console.log(dom_path)
       chrome.runtime.sendMessage({action: "create_new_section", sectionData: {
         url: window.location.href,
         dom_path: dom_path,
         content: content
-      }});
+      }})
       toastr.success('We\'re now monitoring this section')
       selecting = false
     }
@@ -38,25 +38,25 @@ function removeAllHighlights() { $("*").removeClass('updateNotifierSelected') }
 
 function getFullPath(el) {
   if (!(el instanceof Element))
-      return 'hello world';
-  var path = [];
+      return 'hello world'
+  var path = []
   while (el.nodeType === Node.ELEMENT_NODE) {
-      var selector = el.nodeName.toLowerCase();
+      var selector = el.nodeName.toLowerCase()
       if (el.id) {
-          selector += '#' + el.id;
-          path.unshift(selector);
-          break;
+          selector += '#' + el.id
+          path.unshift(selector)
+          break
       } else {
-          var sib = el, nth = 1;
+          var sib = el, nth = 1
           while (sib = sib.previousElementSibling) {
               if (sib.nodeName.toLowerCase() == selector)
-                 nth++;
+                 nth++
           }
           if (nth != 1)
-              selector += ":nth-of-type("+nth+")";
+              selector += ":nth-of-type("+nth+")"
       }
-      path.unshift(selector);
-      el = el.parentNode;
+      path.unshift(selector)
+      el = el.parentNode
   }
-  return path.join(" > ");
+  return path.join(" > ")
 }
