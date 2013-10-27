8727890be780f77abec9f184d1adb48e0beb3ce7
Ryan Endacott <rzeg24@gmail.com>
2013-10-26 21:19:55 -0500

    name

--- assets/js/options.js
+++ assets/js/options.js
@@ -2,8 +2,10 @@ $(function() {
   $('button').click(function () {
     localStorage.setItem('phoneNumber', $('#phone').val())
     localStorage.setItem('email', $('#email').val())
-  	toastr.success('You\'re information has been saved!')
+    localStorage.setItem('name', $('#name').val())
+  	toastr.success('Your information has been saved!')
   })
   $('#phone').val(localStorage.phoneNumber)
   $('#email').val(localStorage.email)
+  $('#name').val(localStorage.name)
 })
